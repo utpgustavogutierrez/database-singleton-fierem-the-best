65e4150da7d4041fdfee3b9f23a56184305de026
alvarocotes <150873826+alvarocotes@users.noreply.github.com>
2024-04-07 15:46:51 -0500

    Update Sakila.java
    implementacion de singleton

--- Sakila.java
+++ Sakila.java
@@ -6,10 +6,8 @@ import java.sql.Statement;
 
 public class Sakila
 {
-  public static void main(String[] args)
+  private Sakila()
   {
-    // NOTE: Connection and Statement are AutoClosable.
-    //       Don't forget to close them both in order to avoid leaks.
     try
     (
       // create a database connection
@@ -32,5 +30,18 @@ public class Sakila
       // it probably means no database file is found
       e.printStackTrace(System.err);
     }
+  }
+
+  public static sincronizacion Sakila getInstance(){
+      if(instance == null) {
+          instance = new Sakila();
+      }
+    return instance;
+  }
+
+  public static void main(String[] args)
+  {
+    Sakila database = Sakila.getInstance();
+    System.out.println(database.Sakila())
   }
 }
